6e27e92b6c6503fe011411bdb0f5ea217799d408
kelvintaywl <kelvintaywl@gmail.com>
2018-11-18 13:02:00 +0900

    fix: lint

--- config.py
+++ config.py
@@ -6,10 +6,14 @@
 #   date in YYYY-MM-DD
 #   hour_from in 24H format
 #   hour_to in 24H format
-RESERVATION_FORM_URL = 'https://labola.jp/reserve/facility/{}/reserve/normal/{}-{}-{}'
+RESERVATION_FORM_URL = (
+    'https://labola.jp/reserve/facility/',
+    '{}/reserve/normal/{}-{}-{}'
+)
 
 INDOOR_COURT_NO = 2024
 OUTDOOR_COURT_NO = 2025
+
 
 def get_url(date, hour_from, hour_to, indoor=True):
     return RESERVATION_FORM_URL.format(
--- groundstaff.py
+++ groundstaff.py
@@ -1,10 +1,8 @@
 import logging
 
 import click
-import requests
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
 from config import get_url
@@ -15,25 +13,31 @@ def getWebDriver():
     # opts.add_argument('--headless')
     return webdriver.Chrome(chrome_options=opts)
 
-# FIXME: temporarily set as global object 
-driver = None
 
-def step1_login_reserve(
-    username, password,
-    date, hour_from, hour_to, is_indoor):
+def step1_login_reserve(driver,
+                        username, password,
+                        date, hour_from, hour_to,
+                        is_indoor):
 
     url = get_url(date, hour_from, hour_to, is_indoor)
     driver.get(url)
 
     try:
-        input_member_code = driver.find_element_by_id('input_reserver_code')
-        input_password = driver.find_element_by_id('input_reserver_password')
-        input_submit = driver.find_element_by_css_selector("input[type='submit']")
-    except NoSuchElementException as e:
+        input_member_code = driver.find_element_by_id(
+            'input_reserver_code'
+        )
+        input_password = driver.find_element_by_id(
+            'input_reserver_password'
+        )
+        input_submit = driver.find_element_by_css_selector(
+            "input[type='submit']"
+        )
+    except NoSuchElementException:
         # NOTE: ASSUMPTION specified time is unavailable
         # of course, this could be the case of form HTML changed
         logging.exception(
-            f'step1: specified time for reservation unavailable: {date}-{hour_from}-{hour_to}'
+            ('step1: specified time for reservation unavailable: ',
+             f'{date}-{hour_from}-{hour_to}')
         )
         return
     else:
@@ -41,18 +45,24 @@ def step1_login_reserve(
         input_password.send_keys(password)
         input_submit.click()
 
-def step2_confirm(mobile_phone_number):
+
+def step2_confirm(driver, mobile_phone_number):
     try:
-        input_mobile_no = driver.find_element_by_id('input_reserver_tel_mobile')
-        input_submit = driver.find_element_by_css_selector("input[type='submit' name='submit_conf']")
+        input_mobile_no = driver.find_element_by_id(
+            'input_reserver_tel_mobile'
+        )
+        input_submit = driver.find_element_by_css_selector(
+            "input[type='submit' name='submit_conf']"
+        )
     except NoSuchElementException:
         logging.exception(
-            f'step2: specified time for reservation unavailable: {mobile_phone_number}'
+            ('step2: specified time for reservation unavailable: ',
+             f'{mobile_phone_number}')
         )
         return
     else:
         input_mobile_no.send_keys(mobile_phone_number)
-        input_submit.click()   
+        input_submit.click()
 
 
 @click.command()
@@ -61,19 +71,20 @@ def step2_confirm(mobile_phone_number):
 @click.option('--date', help='date in YYYY-MM-DD')
 @click.option('--timerange', help='for instance, 1800-2000')
 @click.option('--tel', help='mobile tel number to reserve in')
-@click.option('--indoor/--outdoor', default=False, help='set this if booking indoor court; defaults to outdoor court')
+@click.option('--indoor/--outdoor', default=False,
+              help='set --indoor or --outdoor; defaults to outdoor')
 def main(username, password, date, timerange, tel, indoor):
     try:
         hour_from, hour_to = timerange.split('-')
         # TODO: validation on date & timerange
 
         driver = getWebDriver()
-
-        step1_login_reserve(username, password, date, hour_from, hour_to, indoor)
-        step2_confirm(mobile_phone_number)
+        step1_login_reserve(
+            driver, username, password, date, hour_from, hour_to, indoor
+        )
+        step2_confirm(driver, tel)
     finally:
         driver.close()
-    
 
 
 if __name__ == '__main__':
